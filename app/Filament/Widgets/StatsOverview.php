83fff9fc9727805c7f446dad1bde64808db600e1
Fábio Assunção <fabio23gt@gmail.com>
2023-07-06 04:40:18 -0300

    CS Fixes

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,11 +2,7 @@
 
 namespace App\Filament\Widgets;
 
-use App\Models\Item;
-use App\Models\User;
-use App\Models\Vote;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
-use Filament\Widgets\StatsOverviewWidget\Card;
 
 class StatsOverview extends BaseWidget
 {
