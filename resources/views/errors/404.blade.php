7fc9f449f82e3dfbf3a762789f6b64bfcb8e25b2
Fábio Assunção <fabio23gt@gmail.com>
2023-07-09 17:37:07 -0300

    Visual improvement on the 404 error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,15 @@
 @section('title', 'Page not found')
 
 <x-app>
-    <p>We're unable to find this page.</p>
+    <div class="flex flex-col items-center">
+        <h1 class="font-bold text-blue-600 text-6xl">{{ __('404') }}</h1>
+
+        <h6 class="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+            {{ __('Oops! Page not found') }}
+        </h6>
+
+        <p class="mb-8 text-center text-gray-500 md:text-lg">
+            {{ __('We\'re unable to find this page.') }}
+        </p>
+    </div>
 </x-app>
