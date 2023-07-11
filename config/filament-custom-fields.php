e469aaab01f7e197e18215b8715c5817ac10e074
Fábio Assunção <fabio23gt@gmail.com>
2023-07-10 23:31:40 -0300

    Defining models for custom fields

--- config/filament-custom-fields.php
+++ config/filament-custom-fields.php
@@ -9,7 +9,10 @@ return [
         CustomFieldResponseResource::class,
     ],
     'models' => [
-        //        \App\Models\Trying::class => "trying",
+        \app\Models\Project::class => 'Project',
+        \app\Models\Item::class => 'Item',
+        \app\Models\Ticket::class => 'Ticket',
+        \app\Models\User::class => 'User',
     ],
     'navigation_group' => 'Settings',
     'custom_fields_label' => 'Custom Fields',
